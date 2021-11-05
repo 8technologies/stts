d481089d7042fbb695636da59140c33ca2afa3c4
Muhindo Mubaraka <mubs0x@gmail.com>
2021-11-05 14:36:02 +0300

    new changes

--- database/migrations/2021_11_04_120830_create_form_crop_declarations_table.php
+++ database/migrations/2021_11_04_120830_create_form_crop_declarations_table.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Crop;
+use App\Models\CropVariety;
+use App\Models\FormQds;
 use App\Models\PlantingReturn;
 use Encore\Admin\Auth\Database\Administrator;
 use Illuminate\Database\Migrations\Migration;
@@ -16,22 +18,25 @@ class CreateFormCropDeclarationsTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('form_crop_declarations');
         Schema::create('form_crop_declarations', function (Blueprint $table) {
-            $table->id();
+            $table->id(); 
             $table->timestamps();
 
             $table->foreignIdFor(Administrator::class);
 
-            $table->foreignIdFor(PlantingReturn::class);
+            $table->foreignIdFor(FormQds::class);
 
-            $table->foreignIdFor(Crop::class);
+            $table->foreignIdFor(CropVariety::class);
 
             $table->text("source_of_seed")->nullable();
             $table->text("field_size")->nullable();
             $table->text("seed_rate")->nullable();
             $table->text("amount")->nullable();
             $table->text("payment_receipt")->nullable();
-
+            $table->string('status')->nullable();
+            $table->integer('inspector')->nullable();
+            $table->text('status_comment')->nullable();
         });
     }
 
