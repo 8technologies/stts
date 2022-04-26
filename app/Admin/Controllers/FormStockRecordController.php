a9d133cf1a34c1d36e064fd0acf794029539f206
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-04-26 10:54:15 +0300

    romina

--- app/Admin/Controllers/FormStockRecordController.php
+++ app/Admin/Controllers/FormStockRecordController.php
@@ -210,10 +210,9 @@ class FormStockRecordController extends AdminController
 
         $user = Admin::user();
         $stock = [];
-        $stock_ids = [];
+        $stock_ids = []; 
         $vars = [];
         $recs = StockRecord::where('administrator_id', Admin::user()->id)->get();
-
         foreach ($recs as $key => $value) {
             $stock[$value->crop_variety_id][] = $value;
             if (!isset($stock_ids[$value->crop_variety_id])) {
@@ -223,15 +222,14 @@ class FormStockRecordController extends AdminController
             $stock_ids[$value->crop_variety_id]['quantity'] += $value->quantity;
         }
 
-        $headers = ['#', 'Variety', 'Quantity (in Metric tonnes)',];
-        $rows = [];
-        $i = 0;
+      
         foreach ($stock_ids as $key => $value) {
             if (!isset($value['variety']->id)) {
                 continue;
             }
 
             if ($value['quantity'] > 0) {
+                dd($value['variety']->crop);
                 $vars[$value['variety']->id] = "CROP: " . $value['variety']->crop->name .
                     ", VARIETY: " . $value['variety']->name
                     . ", QUANTITY: " . number_format($value['quantity']);
