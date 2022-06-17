0334454899081452bb0dc4fd7f8706e6e39e5d30
Ntaryebwa Mukama <ntaryebwa.mukama@gmail.com>
2022-06-17 17:08:02 +0300

    3

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,10 +1,10 @@
 <?php
- 
+
 namespace App\Providers;
- 
+
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
- 
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -13,9 +13,9 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Models\Model' => 'App\Policies\ModelPolicy',
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
- 
+
     /**
      * Register any authentication / authorization services.
      *
@@ -24,5 +24,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        //
     }
 }
