30cf15d1f816c781ba1d6d02afab222edc55002c
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-07-27 10:49:36 +0300

    romina

--- app/Models/SubGrower.php
+++ app/Models/SubGrower.php
@@ -133,7 +133,7 @@ class SubGrower extends Model
                                 $d['status_comment'] = "";
                                 $d['planting_return_id'] = $sr10->id;
                                 $d['administrator_id'] = $sr10->administrator_id;
-                                $d['inspector'] =  Admin::user()->id;
+                                $d['inspector'] =  $sr10->inspector;
                                 $date_planted = Carbon::parse($inspe->date_planted);
                                 $date_planted->addDays($inspe->period_after_planting);
                                 $toDateString = $date_planted->toDateString();
@@ -164,7 +164,8 @@ class SubGrower extends Model
                         $d['is_initialized'] = false;
                         $d['status_comment'] = "";
                         $d['planting_return_id'] = $sr10->id;
-                        $d['administrator_id'] = $sr10->inspector;
+                        $d['administrator_id'] = $sr10->administrator_id;
+                        $d['inspector'] =  $sr10->inspector;
                         $date_planted = Carbon::parse(time());
                         $date_planted->addDays(1);
                         $toDateString = $date_planted->toDateString();
