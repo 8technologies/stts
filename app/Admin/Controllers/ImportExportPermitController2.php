6b4e96475574de9fe1d880f23f753964d22ef381
Ntaryebwa Mukama <ntaryebwa.mukama@gmail.com>
2022-08-09 19:26:29 +0300

    import / export application forms fixed

--- app/Admin/Controllers/ImportExportPermitController2.php
+++ app/Admin/Controllers/ImportExportPermitController2.php
@@ -410,8 +410,6 @@ class ImportExportPermitController2 extends AdminController
             //     ->value($seed_board_registration_number);
 
 
-
-
             $form->text('store_location', __('Location of the store'))->required();
             $form->text(
                 'quantiry_of_seed', 
