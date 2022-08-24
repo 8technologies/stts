a29c3d078a7e927d01fb7a4956725854eadbf3dc
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-24 12:03:35 +0300

    uploads

--- app/Models/Utils.php
+++ app/Models/Utils.php
@@ -476,7 +476,7 @@ class Utils
             ) {
                 $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
                 $file_name = time() . "-" . rand(100000, 1000000) . "." . $ext;
-                $destination = 'public/storage/' . $file_name;
+                $destination = 'public/storage/uploads/' . $file_name;
 
                 $res = move_uploaded_file($file['tmp_name'], $destination);
                 if (!$res) {
