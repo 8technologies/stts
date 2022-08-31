e69b08056b6674551d4c1f2fb47d21576c774361
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2022-08-31 11:56:43 +0300

    examination form picker in seedlab

--- app/Http/Controllers/Api/SeedLabApiController.php
+++ app/Http/Controllers/Api/SeedLabApiController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\SeedLab;
-use Encore\Admin\Controllers\AdminController; 
+use Encore\Admin\Controllers\AdminController;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\DB;
 use App\Traits\ApiResponser;
 use Illuminate\Support\Facades\Auth;
 use App\Models\SubGrower;
@@ -13,7 +13,7 @@ use App\Models\Utils;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
-    
+
 class SeedLabApiController extends AdminController
 {
     use ApiResponser;
@@ -30,11 +30,11 @@ class SeedLabApiController extends AdminController
         $user = auth()->user();
         $query = DB::table('seed_labs')->where('administrator_id', '=', $user->id)->get();
         // $query = SeedLab::all();
-        
-        return $this->successResponse($query, $message="Seed Labs"); 
-    } 
 
-      
+        return $this->successResponse($query, $message = "Seed Labs");
+    }
+
+
     // create seed lab create via api
     public function seed_lab_create(Request $request): \Illuminate\Http\JsonResponse
     {
@@ -42,32 +42,33 @@ class SeedLabApiController extends AdminController
 
         $data = $request->only(
             'form_stock_examination_request_id',
-            'collection_date', 
-            'payment_receipt',
+            'collection_date',
             'applicant_remarks',
         );
 
         $post_data = Validator::make($data, [
             'form_stock_examination_request_id',
-            'collection_date' => 'required', 
-            'payment_receipt' => 'required', 
+            'collection_date' => 'required',
             'applicant_remarks' => 'required',
         ]);
 
+
         if ($post_data->fails()) {
-            return $this->errorResponse("Seed Lab submit error", 200); 
+            return $this->errorResponse("Seed Lab submit error", 200);
         }
+        $payment_receipt = Utils::upload_images_1($_FILES, true);
+
 
         $form = SeedLab::create([
             'administrator_id' => $user->id,
             'form_stock_examination_request_id' => $request->input('form_stock_examination_request_id'),
-            'collection_date' => $request->input('collection_date'), 
-            'payment_receipt' => $request->input('payment_receipt'),
+            'collection_date' => $request->input('collection_date'),
+            'payment_receipt' => $payment_receipt,
             'applicant_remarks' => $request->input('applicant_remarks'),
         ]);
 
         // Form created, return success response
-        return $this->successResponse($form, "Seed Lab submit success!", 201); 
+        return $this->successResponse($form, "Seed Lab submit success!", 201);
     }
 
 
