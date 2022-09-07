44b53529a01ac73ef720def112e4ade8c26ab321
Ntaryebwa Mukama <ntaryebwa.mukama@gmail.com>
2022-09-07 19:55:10 +0300

    track / trace sorted

--- app/Admin/Controllers/TrackAndTraceController.php
+++ app/Admin/Controllers/TrackAndTraceController.php
@@ -40,8 +40,9 @@ class TrackAndTraceController extends AdminController
      */
     protected function grid()
     {
-        $grid = new Grid(new SeedLab());        
+        $grid = new Grid(new SeedLab());    
         $grid->column('id', __('Id'));
+        // $grid->column('', __(''));
         $grid->column('lot_number', __('Lot number'))->badge($style = 'blue')->copyable();
         $grid->column('mother_lot', __('Mother lot'))->badge($style = 'green')->copyable();     
 
@@ -50,10 +51,21 @@ class TrackAndTraceController extends AdminController
             $search_param->like('lot_number', __("Search by Lot number"));
             $search_param->like('mother_lot', __("Search by Mother Lot"));
         });
+        
+        $grid->disableCreateButton();
+        $grid->disableRowSelector();
+        $grid->disableColumnSelector();
+        $grid->disableExport();
+
+        
+        $grid->actions(function ($actions) {
+            $actions->disableDelete();
+            $actions->disableEdit();
+        });
+
 
         return $grid;
     }
-
 
     
     /**
@@ -69,21 +81,23 @@ class TrackAndTraceController extends AdminController
          */
         $show = new Show(SeedLab::findOrFail($id));
         
-        $show->panel()->tools(function ($tools) {
-                $tools->disableEdit();
-                $tools->disableDelete();
-        });
-
-        $ser = FormStockExaminationRequest::findOrFail(2);
         
+        if (!Admin::user()->isRole('admin') && !Admin::user()->isRole('super-admin')) {
+            $show->panel()->tools(function ($tools) {
+                    $tools->disableEdit();
+                    $tools->disableDelete();
+            });
+        }
+
+        $ser = FormStockExaminationRequest::findOrFail(2); 
 
         $show->field('id', __('Id'));
-        $show->field('lot_number', __('Lot number'))->badge($style = 'blue');
-        $show->field('mother_lot', __('Mother lot'))->badge($style = 'green');
-        $show->field($ser->company, __('Company Name'));
-        $show->field('company', __('Crop Variety'));
-        $show->field('company', __('Years of Experience'));
-        $show->field('company', __('Original Quantity'));
+        $show->field('lot_number', __('Lot number'));
+        $show->field('mother_lot', __('Mother lot'));
+        $show->field('form_stock_examination_request_id', __('Company Name'));
+        $show->field('form_stock_examination_request_id', __('Crop Variety'));
+        $show->field('form_stock_examination_request_id', __('Year'));
+        $show->field('form_stock_examination_request_id', __('Original Quantity'));
 
 
         return $show;
@@ -91,19 +105,19 @@ class TrackAndTraceController extends AdminController
     }
 
 
-    protected function form()
-    {
-        $form = new Form(new SeedLab());
+    // protected function form()
+    // {
+    //     $form = new Form(new SeedLab());
 
-        $form->display('id', 'ID');
+    //     $form->display('id', 'ID');
 
-        $form->select('parent_id')->options(SeedLab::selectOptions());
+    //     $form->select('parent_id')->options(SeedLab::selectOptions());
 
-        $form->text('mother_lot')->rules('required');
+    //     $form->text('mother_lot')->rules('required');
 
-        //$form->image('logo');
+    //     //$form->image('logo');
 
 
-        return $form;
-    }
+    //     return $form;
+    // }
 }
