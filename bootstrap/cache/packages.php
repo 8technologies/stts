c8f8692ad1da8b72a8e24f785b41b21f9d0866d0
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-01 12:44:34 +0300

    new change

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -134,13 +134,6 @@
       0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
     ),
   ),
-  'laravel/telescope' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Laravel\\Telescope\\TelescopeServiceProvider',
-    ),
-  ),
   'laravel/tinker' => 
   array (
     'providers' => 
