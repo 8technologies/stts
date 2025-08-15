aa8f918d026936f7ca3c69cfd5043201b27491ef
hildah <98084446+HildahN@users.noreply.github.com>
2025-08-15 15:38:41 +0300

    cache clear

--- storage/framework/views/78fedd94487da0e948c01032db236c5eaab1c794.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot, false); ?>: <?php echo e($url, false); ?>
-
-<?php /**PATH C:\Users\Cole\Desktop\stts\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
--- storage/framework/views/b08edac74cec16b23e68ee276908835c6cd531b5.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot, false); ?>
-
-<?php /**PATH C:\Users\Cole\Desktop\stts\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
